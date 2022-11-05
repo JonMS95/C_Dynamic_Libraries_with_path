184992a6cf4e4bfc9f3bffd4e0321602ab734e1d
Jon Ubuntu <jon_portugalete@hotmail.com>
2022-11-05 21:23:13 +0100

    Added some files.

---
+++ Source_files/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include "functionPrototype.h"
+
+#define CORRECT_ARGC_VALUE      3
+#define ARGUMENT_NUMBER_MESSAGE "Only two arguments should be passed on the command line!\r\n"
+#define NON_ZERO_MESSAGE        "Both arguments should be non-zero integers!\r\n"
+
+#endif
